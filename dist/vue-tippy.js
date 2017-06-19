c7d79ce5335bcd6b4b7cc17613e3523d3da73584
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2017-06-19 19:30:00 +0300

    Update...

--- dist/vue-tippy.js
+++ dist/vue-tippy.js
@@ -3694,7 +3694,7 @@ var VueTippy = {
     install: function (Vue) {
         Vue.directive('tippy', {
             bind: function (el, binding, vnode) {
-               const instance = new Tippy(el, {
+                const instance = new Tippy(el, {
                     shown: function () {
                         const handlers = (vnode.data && vnode.data.on) ||
                             (vnode.componentOptions && vnode.componentOptions.listeners);
@@ -3703,12 +3703,13 @@ var VueTippy = {
                             handlers["shown"].fns();
                         }
                     },
-                    wait: function(show, event) {
+                    wait: function (show, event) {
                         // Change your app model to be rendered by the UI library/framework
 
                         // Queue show to run once rendering is complete
-                        setTimeout(function() {
+                        setTimeout(function () {
                             // update(popper) content
+
                             const popper = instance.getPopperElement(el);
 
                             instance.update(popper);
--- vue-tippy.js
+++ vue-tippy.js
@@ -5,7 +5,7 @@ var VueTippy = {
     install: function (Vue) {
         Vue.directive('tippy', {
             bind: function (el, binding, vnode) {
-               const instance = new Tippy(el, {
+                const instance = new Tippy(el, {
                     shown: function () {
                         const handlers = (vnode.data && vnode.data.on) ||
                             (vnode.componentOptions && vnode.componentOptions.listeners);
@@ -14,12 +14,13 @@ var VueTippy = {
                             handlers["shown"].fns();
                         }
                     },
-                    wait: function(show, event) {
+                    wait: function (show, event) {
                         // Change your app model to be rendered by the UI library/framework
 
                         // Queue show to run once rendering is complete
-                        setTimeout(function() {
+                        setTimeout(function () {
                             // update(popper) content
+
                             const popper = instance.getPopperElement(el);
 
                             instance.update(popper);
