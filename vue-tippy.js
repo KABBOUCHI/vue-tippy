d5d3493c1518e060ab311b6f6e6a076f1b0af6c6
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2017-07-08 23:06:46 +0300

    v0.2.0

--- vue-tippy.js
+++ vue-tippy.js
@@ -3,17 +3,39 @@ const css = require('tippy.js/dist/tippy.css');
 
 var VueTippy = {
     install: function (Vue) {
+        Vue.$tippyInstances = [];
         Vue.prototype.$tippy = {
 
             update: function (el) {
-                var instance = Tippy(el);
-                instance.update(instance.getPopperElement(instance.selector))
+                this.instance = Vue.$tippyInstances.find(function ($instance) {
+                    return $instance.el == el;
+                });
+                if (this.instance) {
+                    this.instance.tippy.update(this.instance.tippy.getPopperElement(this.instance.tippy.selector))
+                }
+
+            },
+            forceUpdateHtml: function () {
+                Vue.$tippyInstances.forEach(function (instance) {
+
+                    instance.tippy.store.forEach(function (s) {
+
+                        if (s.settings.html) {
+                            Vue.nextTick(function () {
+                                s.tippyInstance.update(s.popper);
+                            });
+                        }
+
+                    });
+
+                });
             }
 
         };
 
         Vue.directive('tippy', {
             inserted: function (el, binding, vnode, oldVnode) {
+
                 el.tippy = new Tippy(el, {
                     onShown: function () {
 
@@ -26,14 +48,26 @@ var VueTippy = {
                         }
                     },
                 });
+                Vue.$tippyInstances.push({
+                    el: el,
+                    tippy: el.tippy,
+
+                })
+
             },
             componentUpdated: function (el) {
 
                 if (el.tippy && (el.getAttribute('title') || el.getAttribute('data-html'))) {
 
-                    const popper = el.tippy.getPopperElement(el);
+                    el.tippy.store.forEach(function (s) {
 
-                    el.tippy.update(popper);
+                        var popper = s.popper;
+
+                        var tip = s.tippyInstance;
+
+                        tip.update(popper);
+
+                    });
 
                 }
 
