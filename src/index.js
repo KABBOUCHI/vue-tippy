758e0f10bc28c911aacecbb017a907fc7f2789d4
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2019-03-19 19:58:53 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import tippy from 'tippy.js'
 
 const plugin = {
-  install(Vue, options) {
+  install (Vue, options) {
     window.tippy = tippy
     tippy.setDefaults(options || {})
     Vue.component('tippy', require('./components/Tippy.vue'))
 
-    function createTippy(el, binding, vnode) {
+    function createTippy (el, binding, vnode) {
       const handlers = (vnode.data && vnode.data.on) ||
         (vnode.componentOptions && vnode.componentOptions.listeners)
 
@@ -65,15 +65,15 @@ const plugin = {
     }
 
     Vue.directive('tippy', {
-      inserted(el, binding, vnode) {
+      inserted (el, binding, vnode) {
         Vue.nextTick(() => {
           createTippy(el, binding, vnode)
         })
       },
-      unbind(el) {
+      unbind (el) {
         el._tippy && el._tippy.destroy()
       },
-      componentUpdated(el, binding, vnode) {
+      componentUpdated (el, binding, vnode) {
         const opts = binding.value || {}
 
         if (el.getAttribute('title') && !opts.content) {
