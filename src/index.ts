c58e47df740f7a892fa21095b9da8c6daab226d4
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2020-08-02 19:43:29 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import tippy from 'tippy.js'
+import tippy, {
+  sticky,
+  inlinePositioning,
+  followCursor,
+  animateFill,
+} from 'tippy.js'
 import Tippy from './components/Tippy'
 import { useTippy } from './composables/useTippy'
 import { TippyOptions } from './types'
 import 'tippy.js/dist/tippy.css'
 
 const setDefaultProps = tippy.setDefaultProps
+
+setDefaultProps({
+  plugins: [sticky, inlinePositioning, followCursor, animateFill],
+})
 
 export { useTippy, tippy, setDefaultProps, Tippy }
 export { TippyOptions }
