3f3aa41dd07d79a5b2e0b826b258f6109d6db255
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2020-09-27 20:05:10 +0300

    fix

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -5,7 +5,7 @@ import tippy from 'tippy.js'
 import { TippyPluginOptions } from '../types'
 
 const plugin: Plugin = {
-  install(app, options: TippyPluginOptions) {
+  install(app, options: TippyPluginOptions = {}) {
     tippy.setDefaultProps(options.defaultProps || {})
 
     app.directive(options.directive || 'tippy', directive)
