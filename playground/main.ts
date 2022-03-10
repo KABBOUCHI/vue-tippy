bdbb55fe96443556224a7e95c6cece0da84185d9
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-03-10 13:15:13 +0200

    add toggle theme example

--- playground/main.ts
+++ playground/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import VueTippy from '../src'
 import "tippy.js/dist/tippy.css";
+import 'tippy.js/themes/light.css'
+import 'tippy.js/themes/material.css'
 import App from './App.vue'
 import PageIndex from './pages/Index.vue'
 import PageNestedComponents from './pages/NestedComponents.vue'
@@ -11,6 +13,7 @@ import PageSingletonComponents from './pages/SingletonComponents.vue'
 import ReactiveProps from './pages/ReactiveProps.vue'
 import Testing from './pages/Testing.vue'
 import ReactiveState from './pages/ReactiveState.vue'
+import Theme from './pages/Theme.vue'
 import Counter from './components/Counter.vue'
 import UiIcon from "./components/Icon.vue";
 
@@ -23,6 +26,7 @@ const router = createRouter({
     { path: '/testing', component: Testing },
     { path: '/reactive-props', component: ReactiveProps },
     { path: '/reactive-state', component: ReactiveState },
+    { path: '/theme', component: Theme },
   ]
 })
 
---
+++ playground/pages/Theme.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <tippy
+      content="Hello!"
+      :theme="theme"
+      sticky
+      :showOnCreate="true"
+      :hideOnClick="false"
+      trigger="manual"
+    >
+      <button class="text-sm py-2 px-3 bg-gray-900 text-white rounded-lg">
+        Theme {{ theme }}
+      </button>
+    </tippy>
+
+    <br />
+
+    <button
+      @click="toggleTheme"
+      class="mt-4 text-sm py-2 px-3 bg-gray-900 text-white rounded-lg"
+    >
+      Toggle Theme
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      theme: 'light',
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light'
+    },
+  },
+}
+</script>
